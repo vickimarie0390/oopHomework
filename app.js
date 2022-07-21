eab7a019485e989a4e9fa1379cc30cd068f9489a
Victoria <vickimarji@icloud.com>
2022-07-21 06:20:05 -0400

    ninja

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+var Car = function(maxSpeed, driver){
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time){
+        console.log(speed * time);
+    };
+    this.logDriver = function(){
+        console.log("driver name is " + this.driver);
+    };
+}
+
+var myCar = new Car(70, "Ninja Man");
+var myCar2 = new Car(40, "Humpty Dumpty");
+var myCar3 = new Car(10, "Shaun");
+var myCar4 = new Car(90, "James Bond");
+myCar.drive(30,5);
+myCar3.logDriver();
